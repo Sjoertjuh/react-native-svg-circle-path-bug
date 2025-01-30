ed8c6065e47152ecf0e69e5a9b32d82a78922532
Sjoertjuh <63722509+Sjoertjuh@users.noreply.github.com>
2025-01-30 11:05:40 +0100

    add example that path does work

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { View } from 'react-native';
-import Svg, { Circle, Text, TextPath } from 'react-native-svg';
+import Svg, { Circle, Path, Text, TextPath } from 'react-native-svg';
 
 export default function HomeScreen() {
   return (
@@ -91,6 +91,16 @@ export default function HomeScreen() {
             <TextPath href="#textAlignmentHelper" startOffset="100%">
                 End end text
             </TextPath>
+        </Text>
+      </Svg>
+
+      <Svg width={200} height={100} viewBox="0 0 500 250">
+        <Path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
+
+        <Text textAnchor="end" fill="#FFF" fontSize={26}>
+          <TextPath alignment-baseline="top" href="#curve" startOffset="100%">
+            This does work
+          </TextPath>
         </Text>
       </Svg>
     </View>
