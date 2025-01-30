cdd2c4b225ae351f22bec08a4728986f4e259c75
Sjoertjuh <63722509+Sjoertjuh@users.noreply.github.com>
2025-01-30 10:42:58 +0100

    improve example

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,34 @@
-import { Image, StyleSheet, Platform, View } from 'react-native';
+import { View } from 'react-native';
 import Svg, { Circle, Text, TextPath } from 'react-native-svg';
 
 export default function HomeScreen() {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#333'}}>
       <Svg width={200} height={200} viewBox='0 0 200 200'>
         <Circle
           id="textAlignmentHelper"
           cx={100}
           cy={100}
           r={80}
-          fill="#ff0000a0"
+          fill="#ff000050"
         />
 
         <Text textAnchor="start" fill="#FFF">
             <TextPath href="#textAlignmentHelper">
-                Start example text
+                Start start text
+            </TextPath>
+        </Text>
+
+        <Text textAnchor="start" fill="#FFF">
+            <TextPath href="#textAlignmentHelper" startOffset="50%">
+                Start halfway text
+            </TextPath>
+        </Text>
+
+        {/* This text bunches up in the end on Android, but is hidden as expected on ios */}
+        <Text textAnchor="start" fill="#FFF">
+            <TextPath href="#textAlignmentHelper" startOffset="100%">
+                Start end text
             </TextPath>
         </Text>
       </Svg>
@@ -26,13 +39,26 @@ export default function HomeScreen() {
           cx={100}
           cy={100}
           r={80}
-          fill="#ff0000a0"
+          fill="#00ff0050"
         />
 
-        {/* This text is not curved along the circle correctly on Android */}
+        {/* This text bunches up in the start on Android, and is cut off as expected on ios */}
         <Text textAnchor="middle" fill="#FFF">
             <TextPath href="#textAlignmentHelper">
-                Middle example text
+                Middle start text
+            </TextPath>
+        </Text>
+
+        <Text textAnchor="middle" fill="#FFF">
+            <TextPath href="#textAlignmentHelper" startOffset="50%">
+                Middle halfway text
+            </TextPath>
+        </Text>
+
+        {/* This text bunches up in the end on Android, and is cut off as expected on ios */}
+        <Text textAnchor="middle" fill="#FFF">
+            <TextPath href="#textAlignmentHelper" startOffset="100%">
+                Middle end text
             </TextPath>
         </Text>
       </Svg>
@@ -43,13 +69,27 @@ export default function HomeScreen() {
           cx={100}
           cy={100}
           r={80}
-          fill="#ff0000a0"
+          fill="#0000ff50"
         />
+
+        {/* This text is not shown at all on Android, and is hidden as expected on ios */}
+        <Text textAnchor="end" fill="#FFF">
+            <TextPath href="#textAlignmentHelper">
+                End start text
+            </TextPath>
+        </Text>
 
         {/* This text is not shown at all on Android */}
         <Text textAnchor="end" fill="#FFF">
-            <TextPath href="#textAlignmentHelper">
-                End example text
+            <TextPath href="#textAlignmentHelper" startOffset="50%">
+                End halfway text
+            </TextPath>
+        </Text>
+
+        {/* This text is not shown at all on Android */}
+        <Text textAnchor="end" fill="#FFF">
+            <TextPath href="#textAlignmentHelper" startOffset="100%">
+                End end text
             </TextPath>
         </Text>
       </Svg>
